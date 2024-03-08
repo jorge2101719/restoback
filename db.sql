af02819385c5a9a3716e71abecd9329fc2aa9d5f
Pipeara <piposo3@hotmail.es>
2024-03-07 21:28:41 -0300

    creando basededatso

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE platos (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(255),
+    descripcion TEXT,
+    precio DECIMAL(10, 2),
+    img TEXT
+);
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
